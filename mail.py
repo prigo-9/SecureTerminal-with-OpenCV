81f75c08ec68b624c7fac774aedafc63e6a6c73b
prigo-9 <48386504+prigo-9@users.noreply.github.com>
2019-12-13 14:27:14 +0530

    mail.py

---
+++ mail.py
@@ -0,0 +1,68 @@
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+   
+fromaddr = "kale.pa@somaiya.edu"
+toaddr = "kale.pa@somaiya.edu"
+   
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+  
+# storing the senders email address   
+msg['From'] = fromaddr 
+  
+# storing the receivers email address  
+msg['To'] = toaddr 
+  
+# storing the subject  
+msg['Subject'] = "Subject of the Mail"
+  
+# string to store the body of the mail 
+body = "Body_of_the_mail"
+  
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+import cv2
+cap=cv2.VideoCapture(0)
+ret,photo=cap.read()
+cv2.imwrite("/root/new.jpg",photo)
+cap.release()
+
+ 
+# open the file to be sent  
+filename = "new.jpg"
+attachment = open("./new.jpg", "rb") 
+  
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+  
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+  
+# encode into base64 
+encoders.encode_base64(p) 
+   
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+  
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(fromaddr, "Ujwala9ashok") 
+  
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+  
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+  
+# terminating the session 
+s.quit() 
